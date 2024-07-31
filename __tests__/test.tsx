81c112184ae83da0c353820cc027b5e2f3bae3cc
Mark Lawlor <mwlawlor@gmail.com>
2024-08-01 08:30:14 +1000

    update test

--- __tests__/test.tsx
+++ __tests__/test.tsx
@@ -1,63 +1,46 @@
-import { useEffect } from "react";
-import { StyleSheet, View } from "react-native";
-import { render } from "@testing-library/react-native";
-import {
+import { Button, View } from "react-native";
+import { fireEvent, render, screen } from "@testing-library/react-native";
+import Animated, {
   useSharedValue,
-  useAnimatedStyle,
-  withRepeat,
-  withTiming,
   getAnimatedStyle,
 } from "react-native-reanimated";
 
-import Animated from "react-native-reanimated";
-
 jest.useFakeTimers();
 
-function App() {
-  const offset = useSharedValue(100);
+test("inline styles", () => {
+  /**
+   * Example taken from https://docs.swmansion.com/react-native-reanimated/docs/fundamentals/your-first-animation#using-a-shared-value
+   */
+  function InlineStyle() {
+    const width = useSharedValue(100);
 
-  const animatedStyles = useAnimatedStyle(
-    () => ({
-      transform: [{ translateX: offset.value }],
-    }),
-    []
-  );
+    const handlePress = () => {
+      width.value = width.value + 50;
+    };
 
-  useEffect(() => {
-    offset.value = withRepeat(
-      withTiming(-offset.value, { duration: 500 }),
-      -1,
-      true
+    return (
+      <View style={{ flex: 1, alignItems: "center" }}>
+        <Animated.View
+          testID="view"
+          style={{
+            width,
+            height: 100,
+            backgroundColor: "violet",
+          }}
+        />
+        <Button testID="button" onPress={handlePress} title="Click me" />
+      </View>
     );
-  }, []);
+  }
 
-  return (
-    <View style={styles.container}>
-      <Animated.View testID="view" style={[styles.box, animatedStyles]} />
-    </View>
-  );
-}
+  render(<InlineStyle />);
+  const view = screen.getByTestId("view");
+  const button = screen.getByTestId("button");
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    height: "100%",
-  },
-  box: {
-    height: 120,
-    width: 120,
-    backgroundColor: "#b58df1",
-    borderRadius: 20,
-  },
-});
+  expect(getAnimatedStyle(view).width).toEqual(100);
 
-test("test reanimated", () => {
-  const { getByTestId } = render(<App />);
-  const view = getByTestId("view");
+  fireEvent.press(button);
 
-  expect(getAnimatedStyle(view).transform).toEqual([{ translateX: 100 }]);
   jest.advanceTimersByTime(500);
-  expect(getAnimatedStyle(view).transform).toEqual([{ translateX: -100 }]);
+  expect(getAnimatedStyle(view).width).toEqual(150);
 });
--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,5 @@
 module.exports = function (api) {
   api.cache(true);
-  // This works
-  // return {
-  //   presets: ["babel-preset-expo"],
-  // };
-
-  // This doesn't work
   return {
     presets: ["module:metro-react-native-babel-preset"],
     plugins: [["@babel/plugin-transform-private-methods", { loose: true }]], // Jest complains is this isn't set
